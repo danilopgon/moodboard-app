9c2807f41b49ec963c7184020744c9fa969fcc91
danilopgon <dani.lopezgonzalez.94@gmail.com>
2023-06-21 20:33:25 +0200

    Temporary main page

---
+++ src/app/components/SwitchTheme.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { themeChange } from "theme-change";
+import { FiMoon, FiSun } from "react-icons/fi";
+
+const SwitchTheme = () => {
+  const [theme, setTheme] = useState();
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+
+  return (
+    <button
+      className="btn btn-circle"
+      onClick={toggleTheme}
+      data-toggle-theme="night,cupcake"
+      data-act-class="ACTIVECLASS"
+    >
+      {theme === "dark" ? (
+        <FiMoon className="w-5 h-5" />
+      ) : (
+        <FiSun className="w-5 h-5" />
+      )}
+    </button>
+  );
+};
+
+export default SwitchTheme;
